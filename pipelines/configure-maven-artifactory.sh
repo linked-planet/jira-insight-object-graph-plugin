6392f1b682ec67dd9d278118be8db30fcdad8baf
Alexander Weickmann <alexander.weickmann@link-time.com>
2019-12-09 19:24:57 +0100

    configure-maven-artifactory.sh: add servers tag if not existing in maven settings file

--- pipelines/configure-maven-artifactory.sh
+++ pipelines/configure-maven-artifactory.sh
@@ -6,6 +6,11 @@ if [ "$#" -lt 1 ]; then
 fi
 
 MAVEN_SETTINGS_FILE=$1
+
+if ! grep -q "<servers>" "$MAVEN_SETTINGS_FILE"; then
+    sed -i~ "/<settings>/ a<servers>\n</servers>" "$MAVEN_SETTINGS_FILE"
+fi
+
 SERVER_XML_RELEASE="<server><id>artifactory.libs-release-local</id><username>$ARTIFACTORY_USERNAME</username><password>$ARTIFACTORY_PASSWORD</password></server>"
 SERVER_XML_SNAPSHOT="<server><id>artifactory.libs-snapshot-local</id><username>$ARTIFACTORY_USERNAME</username><password>$ARTIFACTORY_PASSWORD</password></server>"
 SERVER_LINK_TIME_ARTIFACTORY="<server><id>link-time.artifactory</id><username>$LINK_TIME_ARTIFACTORY_USERNAME</username><password>$LINK_TIME_ARTIFACTORY_PASSWORD</password></server>"
