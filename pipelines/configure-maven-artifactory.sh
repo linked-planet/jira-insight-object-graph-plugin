2086dba03d1630ebad584c933f2188753acfb051
Alexander Weickmann <alexander.weickmann@link-time.com>
2019-12-09 18:57:01 +0100

    fix maven settings file location for travis while still operating under bitbucket pipelines

--- pipelines/configure-maven-artifactory.sh
+++ pipelines/configure-maven-artifactory.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-set -e
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <mavenSettingsFile>"
+    exit 1
+fi
 
-MAVEN_SETTINGS_FILE=/usr/share/maven/conf/settings.xml
+MAVEN_SETTINGS_FILE=$1
 SERVER_XML_RELEASE="<server><id>artifactory.libs-release-local</id><username>$ARTIFACTORY_USERNAME</username><password>$ARTIFACTORY_PASSWORD</password></server>"
 SERVER_XML_SNAPSHOT="<server><id>artifactory.libs-snapshot-local</id><username>$ARTIFACTORY_USERNAME</username><password>$ARTIFACTORY_PASSWORD</password></server>"
 SERVER_LINK_TIME_ARTIFACTORY="<server><id>link-time.artifactory</id><username>$LINK_TIME_ARTIFACTORY_USERNAME</username><password>$LINK_TIME_ARTIFACTORY_PASSWORD</password></server>"
 
-sed -i~ "/<servers>/ a$SERVER_XML_RELEASE" $MAVEN_SETTINGS_FILE
-sed -i~ "/<servers>/ a$SERVER_XML_SNAPSHOT" $MAVEN_SETTINGS_FILE
-sed -i~ "/<servers>/ a$SERVER_LINK_TIME_ARTIFACTORY" $MAVEN_SETTINGS_FILE
+sed -i~ "/<servers>/ a$SERVER_XML_RELEASE" "$MAVEN_SETTINGS_FILE"
+sed -i~ "/<servers>/ a$SERVER_XML_SNAPSHOT" "$MAVEN_SETTINGS_FILE"
+sed -i~ "/<servers>/ a$SERVER_LINK_TIME_ARTIFACTORY" "$MAVEN_SETTINGS_FILE"
