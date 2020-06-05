708e9e9bca624bba6ceef9e706b4751a30ed5fac
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2020-06-05 16:00:31 +0200

    simplify release.sh

--- pipelines/release.sh
+++ pipelines/release.sh
@@ -3,25 +3,18 @@
 set -e
 
 if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <developerConnection>"
+    echo "Usage: $0 <version>"
     exit 1
 fi
 
-DEVELOPER_CONNECTION=$1
+VERSION=$1
 
 get_version_from_maven() {
     mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tail -n 1
 }
 
-mvn build-helper:parse-version versions:set "-DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}" versions:commit
-version=$(get_version_from_maven)
-echo "${version}" > release.version
-git add pom.xml && git commit -m "[skip ci] release v${version}"
-mvn clean deploy scm:tag \
-    "-DaltReleaseDeploymentRepository=artifactory.libs-release-local::$ARTIFACTORY_CONTEXT_URL/libs-release-local" \
-    "-DaltSnapshotDeploymentRepository=artifactory.libs-snapshot-local::$ARTIFACTORY_CONTEXT_URL/libs-snapshot-local" \
-    "-Ddeveloper.connection=scm:git:$DEVELOPER_CONNECTION" \
-    -Pci
+mvn build-helper:parse-version versions:set "-DnewVersion=${VERSION}" versions:commit
+git add pom.xml && git commit -m "release v${VERSION}" && git tag "v${VERSION}" && git push origin master --tags
 mvn build-helper:parse-version versions:set "-DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT" versions:commit
-version=$(get_version_from_maven)
-git add pom.xml && git commit -m "[skip ci] set development version ${version}" && git push
+next_version=$(get_version_from_maven)
+git add pom.xml && git commit -m "[skip ci] set development version ${next_version}" && git push
