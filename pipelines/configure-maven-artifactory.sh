2a607a13696bd55057be6b7f2e6fdf59df520e4a
Alexander Weickmann <alexander.weickmann@link-time.com>
2020-06-05 14:01:59 +0200

    no longer need separate link time artifactory configuration

--- pipelines/configure-maven-artifactory.sh
+++ pipelines/configure-maven-artifactory.sh
@@ -13,8 +13,6 @@ fi
 
 SERVER_XML_RELEASE="<server><id>artifactory.libs-release-local</id><username>$ARTIFACTORY_USERNAME</username><password>$ARTIFACTORY_PASSWORD</password></server>"
 SERVER_XML_SNAPSHOT="<server><id>artifactory.libs-snapshot-local</id><username>$ARTIFACTORY_USERNAME</username><password>$ARTIFACTORY_PASSWORD</password></server>"
-SERVER_LINK_TIME_ARTIFACTORY="<server><id>link-time.artifactory</id><username>$LINK_TIME_ARTIFACTORY_USERNAME</username><password>$LINK_TIME_ARTIFACTORY_PASSWORD</password></server>"
 
 sed -i~ "/<servers>/ a$SERVER_XML_RELEASE" "$MAVEN_SETTINGS_FILE"
 sed -i~ "/<servers>/ a$SERVER_XML_SNAPSHOT" "$MAVEN_SETTINGS_FILE"
-sed -i~ "/<servers>/ a$SERVER_LINK_TIME_ARTIFACTORY" "$MAVEN_SETTINGS_FILE"
